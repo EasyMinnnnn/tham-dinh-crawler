49b11f023385b5ec4f21e905b3c70909aba9a35f
EasyMinnnnn <thanhloc.111991@gmail.com>
2025-07-08 14:32:42 +0700

    Update ocr_to_json.py

--- ocr_to_json.py
+++ ocr_to_json.py
@@ -1,11 +1,20 @@
 # src/ocr_to_json.py
 import os
+import json
 from google.cloud import documentai_v1 as documentai
+from google.protobuf.json_format import MessageToJson
 
 def parse_pdf_with_docai(pdf_path):
-    project_id = "YOUR_PROJECT_ID"
-    location = "us"  # hoặc "eu"
-    processor_id = "YOUR_PROCESSOR_ID"
+    project_id = "geocoding-api-464306"
+    location = "eu" 
+    processor_id = "72cb2ba0beaa1f9d"
+
+    # Load from ENV secret
+    creds_json = os.environ["GOOGLE_APPLICATION_CREDENTIALS_JSON"]
+    with open("gcloud_key.json", "w", encoding="utf-8") as f:
+        f.write(creds_json)
+
+    os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = "gcloud_key.json"
 
     client = documentai.DocumentProcessorServiceClient()
     name = f"projects/{project_id}/locations/{location}/processors/{processor_id}"
@@ -22,4 +31,4 @@ if __name__ == "__main__":
     os.makedirs("outputs", exist_ok=True)
     document = parse_pdf_with_docai("outputs/downloaded.pdf")
     with open("outputs/result.json", "w", encoding="utf-8") as f:
-        f.write(document.__str__())
+        f.write(MessageToJson(document))
